f9c6cf4a723f1e4694fcb141760682ebd87fefae
Alice Mwihaki <alicemwihaki99@gmail.com>
2020-01-13 04:09:22 +0300

    created unittest for testing data

--- main/main.py
+++ main/main.py
@@ -25,13 +25,15 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
     form =Loginform()
+    if form.validate_on_submit():
+        return '<h1> "form.username.date +'' + form.password.data + '<h1>"
     return render_template(login.html, form=form)
 
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET','POST'])
 def login():
     form = Registerform()
     return render_template(signup.html, form=form)
