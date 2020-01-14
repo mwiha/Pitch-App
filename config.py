c816b0e06bc6b31976756390618b5c48416a1ba7
Alice Mwihaki <alicemwihaki99@gmail.com>
2020-01-14 16:22:18 +0300

    did corrections in config

--- config.py
+++ config.py
@@ -1,10 +1,36 @@
+import os
+# from dotenv import load_dotenv
+# load_dotenv()
 class Config:
-    # ....
-    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://Alice:   @localhost/pitchApp'
     
-email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
+    #  email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = "alicemwihaki99@gmail.com"
+    MAIL_PASSWORD = "0798967681"
+    
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    DEBUG = True
+    
+    
+config_options = {
+'development': DevConfig,
+'production': ProdConfig
+}
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
-export MAIL_USERNAME=<alicemwihaki99@gmail.com>
-export MAIL_PASSWORD=<0798967681>
+export MAIL_USERNAME="alicemwihaki99@gmail.com"
+export MAIL_PASSWORD="0798967681"
+SECRET_KEY="Alice"
 python3.6 manage.py server
