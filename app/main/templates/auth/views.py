d378b332d661dcc4e4b498cdea810c104b6b3b1d
Alice Mwihaki <alicemwihaki99@gmail.com>
2020-01-14 10:34:52 +0300

    added start script

--- app/main/templates/auth/views.py
+++ app/main/templates/auth/views.py
@@ -3,6 +3,7 @@ from flask_login import login_user
 from ..models import User
 from .forms import LoginForm,RegistrationForm
 from flask_login import login_user,logout_user,login_required
+from .. import db,photos
 
 
 
@@ -48,3 +49,13 @@ def update_profile(uname):
 
     return render_template('profile/update.html',form =form)
 
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
---
+++ start.sh
@@ -0,0 +1,3 @@
+export MAIL_USERNAME=<alicemwihaki99@gmail.com>
+export MAIL_PASSWORD=<0798967681>
+python3.6 manage.py server
