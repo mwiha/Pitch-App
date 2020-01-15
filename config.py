e404311effea075d376d1687e79094ca1802df41
Alice Mwihaki <alicemwihaki99@gmail.com>
2020-01-15 13:23:56 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -4,19 +4,22 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:alice1234@localhost/alice'
     #  email configurations
+    
+    
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
 class ProdConfig(Config):
     '''
     Production  configuration child class
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:alice1234@localhost/alice'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
   
 class TestConfig(Config):
     '''
